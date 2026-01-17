de1cf00a32f73a7dd7e87fa6ead961a3670ef615
ViniciusZimmermann <145623272+ViniciusZimmermann@users.noreply.gitlab.com>
2026-01-17 16:55:26 -0300

    feat : Implementa SecurityConfig configuração de permissão de rotas permitindo cors api externas permitindo todos fazer e login e registrar caso for outro rota tem que está autenticado e no metodo passwordEnconder vou usar Bcrypt na senha para criptografia que utiliza Salt

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -9,6 +9,7 @@ import com.example.demo.repository.UserRepository;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,9 +20,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public AuthController(UserRepository userRepository){
+    public AuthController(UserRepository userRepository, PasswordEncoder passwordEncoder){
         this.userRepository = userRepository;
+        this.passwordEncoder= passwordEncoder;
     }
 
     @PostMapping("/login")
@@ -33,7 +36,7 @@ public class AuthController {
     public ResponseEntity<RegisterUserResponseDTO> register(@Valid @RequestBody RegisterUserRequestDTO request){
         User newUser = new User();
         newUser.setEmail(request.email());
-        newUser.setPassword(request.password());
+        newUser.setPassword(passwordEncoder.encode(request.password()));
         newUser.setName(request.name());
 
         userRepository.save(newUser);
