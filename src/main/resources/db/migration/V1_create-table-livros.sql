932b5081ba862bd622bba2e355d295edefb636a8
ViniciusZimmermann <145623272+ViniciusZimmermann@users.noreply.gitlab.com>
2026-01-17 20:10:56 -0300

    fix: correção

--- src/main/resources/db/migration/V1_create-table-livros.sql
+++ src/main/resources/db/migration/V1_create-table-livros.sql
@@ -1,6 +1,6 @@
 CREATE TABLE livros (
-   id BIGINT AUTO_INCREMENT PRIMARY KEY,
-   titulo VARCHAR(200) NOT NULL,
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(200) NOT NULL,
     autor VARCHAR(150) NOT NULL,
     ano_publicacao INT
 );
