e89f8463ceb9a7ae2dca606b3dfdf402460f2cd1
ViniciusZimmermann <145623272+ViniciusZimmermann@users.noreply.gitlab.com>
2026-01-17 09:49:42 -0300

    feat : Add metodo create para criar livro salva pela requisição do cliente e retorna a resposta ao cliente

--- src/main/java/com/example/demo/service/LivroService.java
+++ src/main/java/com/example/demo/service/LivroService.java
@@ -1,5 +1,6 @@
 package com.example.demo.service;
 
+import com.example.demo.dto.LivroRequestDTO;
 import com.example.demo.dto.LivroResponseDTO;
 import com.example.demo.model.Livro;
 import com.example.demo.repository.LivroRepository;
@@ -12,17 +13,27 @@ import java.util.List;
 @Service
 public class LivroService {
 
-    public final LivroRepository livroRepository;
+    public final LivroRepository bookRepository;
 
     public LivroService(LivroRepository livroRepo) {
-        this.livroRepository = livroRepo;
+        this.bookRepository = livroRepo;
     }
 
     public List<LivroResponseDTO> findAll() {
-        return livroRepository.findAll()
+        return bookRepository.findAll()
                 .stream()
                 .map(LivroResponseDTO::fromEntity)
                 .toList();
+    }
+
+    public LivroResponseDTO create(LivroRequestDTO livroRequest) {
+        Livro book = new Livro();
+        book.setTitulo(livroRequest.titulo());
+        book.setAutor(livroRequest.autor());
+        book.setAnoPublicacao(livroRequest.anoPublicacao());
+
+        Livro saved = bookRepository.save(book);
+        return LivroResponseDTO.fromEntity(saved);
     }
 
 }
