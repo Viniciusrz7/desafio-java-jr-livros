ee504c03969cc505deda8afb26ba658d3e9c4421
ViniciusZimmermann <145623272+ViniciusZimmermann@users.noreply.gitlab.com>
2026-01-17 17:38:11 -0300

    feat: Implementando geração do token com as variaveis que estão no .properties do ambiente usando algoritomo HMAC256 e setando expiração

---
+++ src/main/java/com/example/demo/config/TokenConfig.java
@@ -0,0 +1,33 @@
+package com.example.demo.config;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.example.demo.model.User;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+
+@Component
+public class TokenConfig {
+
+    @Getter
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Getter
+    @Value("${jwt.expiration}")
+    private Long expiration;
+
+    public String generateToken(User user) {
+        Algorithm algorithm = Algorithm.HMAC256(secret);
+        return JWT.create()
+                .withClaim("userId", user.getId())
+                .withSubject(user.getEmail())
+                .withExpiresAt(Instant.now().plusSeconds(86400))
+                .withIssuedAt(Instant.now())
+                .sign(algorithm);
+    }
+
+}
