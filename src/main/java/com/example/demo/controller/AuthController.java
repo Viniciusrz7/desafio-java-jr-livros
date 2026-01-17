7595d4adcf30feb7498230688b286f8ebb69b0bb
ViniciusZimmermann <145623272+ViniciusZimmermann@users.noreply.gitlab.com>
2026-01-17 16:38:42 -0300

    feat: ADD register do AuthController

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,10 +1,13 @@
 package com.example.demo.controller;
 
 import com.example.demo.dto.request.LoginRequestDTO;
+import com.example.demo.dto.request.RegisterUserRequestDTO;
 import com.example.demo.dto.response.LoginResponseDTO;
 import com.example.demo.dto.response.RegisterUserResponseDTO;
+import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -26,5 +29,15 @@ public class AuthController {
         return null;
     }
 
+    @PostMapping("/register")
+    public ResponseEntity<RegisterUserResponseDTO> register(@Valid @RequestBody RegisterUserRequestDTO request){
+        User newUser = new User();
+        newUser.setEmail(request.email());
+        newUser.setPassword(request.password());
+        newUser.setName(request.name());
 
+        userRepository.save(newUser);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(new RegisterUserResponseDTO(newUser.getName(), newUser.getEmail()));
+    }
 }
