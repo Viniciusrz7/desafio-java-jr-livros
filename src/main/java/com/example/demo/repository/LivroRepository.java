63fa5c1d3ba56daf091a5eebaea5667b7be1231a
ViniciusZimmermann <145623272+ViniciusZimmermann@users.noreply.gitlab.com>
2026-01-17 08:44:23 -0300

    feat: Implementando LivroRepository com 2 metodos buscando os livros pelo titulo e pelo autor

---
+++ src/main/java/com/example/demo/repository/LivroRepository.java
@@ -0,0 +1,16 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.Livro;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface LivroRepository extends JpaRepository<Livro, Long> {
+
+    List<Livro> findByTituloContainingIgnoreCase(String titulo);
+
+    List<Livro> findByAutorContainingIgnoreCase(String autor);
+
+}
