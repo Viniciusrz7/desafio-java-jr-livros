e5cd7576bc28485bb52f6367cb83398f63d1ed7d
ViniciusZimmermann <145623272+ViniciusZimmermann@users.noreply.gitlab.com>
2026-01-17 21:39:50 -0300

    fix: Correção de Imports

--- src/main/java/com/example/demo/config/TokenConfig.java
+++ src/main/java/com/example/demo/config/TokenConfig.java
@@ -2,7 +2,6 @@ package com.example.demo.config;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.example.demo.model.User;
 import org.springframework.beans.factory.annotation.Value;
